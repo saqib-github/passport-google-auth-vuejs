5222c23afef3d3260c0595fc7d0b12635bafde81
saqib <rsaqib2034@gmail.com>
2021-09-07 11:49:14 +0500

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,12 @@
 <template>
-  <g-signin-button
-    :params="googleSignInParams"
-    @success="onSignInSuccess"
-    @error="onSignInError"
-  >
-    Sign in with Google
-  </g-signin-button>
+  <div>
+    
+  </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      /**
-       * The Auth2 parameters, as seen on
-       * https://developers.google.com/identity/sign-in/web/reference#gapiauth2initparams.
-       * As the very least, a valid client_id must present.
-       * @type {Object}
-       */
-      googleSignInParams: {
-        client_id:
-          "528876500955-ub3vdf9pg97jujhaisul2ollnkdqh41n.apps.googleusercontent.com",
-      },
-    };
-  },
-  methods: {
-    onSignInSuccess(googleUser) {
-      // `googleUser` is the GoogleUser object that represents the just-signed-in user.
-      // See https://developers.google.com/identity/sign-in/web/reference#users
-      const profile = googleUser.getBasicProfile(); // etc etc
-      console.log("profile", profile);
-      console.log("profile_id", profile.US);
-      this.$router.push("/about");
-    },
-    onSignInError(error) {
-      // `error` contains any error occurred.
-      console.log("OH NOES", error);
-    },
-  },
+  data() {},
 };
 </script>
 
-<style>
-.g-signin-button {
-  /* This is where you control how the button looks. Be creative! */
-  display: inline-block;
-  padding: 4px 8px;
-  border-radius: 3px;
-  background-color: #3c82f7;
-  color: #fff;
-  box-shadow: 0 3px 0 #0f69ff;
-}
-</style>
+<style></style>
